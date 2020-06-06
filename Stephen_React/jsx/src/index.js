7b1415e3031bcd68cd1ad601b9ad8b2c2309b136
Vidit Shah <0210vidit@gmail.com>
2020-06-07 01:12:01 +0530

    jsx

--- Stephen_React/jsx/src/index.js
+++ Stephen_React/jsx/src/index.js
@@ -3,7 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Create a react component
 const App = function () {
-  return <div>Hi There!</div>;
+  const buttonText = 'Click Me!';
+  return (
+    <div>
+      <label className='label' htmlFor='name'>
+        Enter Name:
+      </label>
+      <input id='name' type='text' />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 //Take the React Component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
