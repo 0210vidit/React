a0f3567fee968baf09511951cd48f2a0dbe9295a
Vidit Shah <0210vidit@gmail.com>
2020-06-08 00:44:25 +0530

    c1

---
+++ Stephen_React/components1/src/CommentDetail.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const CommentDetail = ({ author, timeAgo, image, comment }) => {
+  return (
+    <div className='comment'>
+      <a href='/' className='avatar'>
+        <img alt='avatar' src={image} />
+      </a>
+      <div className='content'>
+        <a href='/' className='author'>
+          {author}
+        </a>
+        <div className='metadata'>
+          <span className='date'>{timeAgo}</span>
+        </div>
+        <div className='text'>{comment}</div>
+      </div>
+    </div>
+  );
+};
--- Stephen_React/components1/src/index.js
+++ Stephen_React/components1/src/index.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import faker from 'faker';
+import { CommentDetail } from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 const App = () => {
   return (
     <div className=' ui container comments'>
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img alt='avatar' />
-        </a>
-        <div className='content'>
-          <a href='/' className='author'>
-            Sam
-          </a>
-          <div className='metadata'>
-            <span className='date'>Today at 6:00pm</span>
-          </div>
-          <div className='text'>Nice Blog Post!</div>
-        </div>
-      </div>
+      <ApprovalCard>Are you sure you want to do this</ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 4:00 pm'
+          comment='Good Post'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Robert'
+          timeAgo='Yesterday at 4:00 pm'
+          comment='Nice Post'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          timeAgo='Today at 6:00 pm'
+          comment='Amzing Post'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
