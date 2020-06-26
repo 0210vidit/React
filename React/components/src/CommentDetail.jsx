e9f3cd7394c0754b919ac8144b638ee2bf36ff25
Vidit Shah <0210vidit@gmail.com>
2020-06-26 21:12:14 +0530

    reformat

--- React/components/src/CommentDetail.jsx
+++ React/components/src/CommentDetail.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
   return (
     <div className='comment'>
       <a href='/' className='avatar'>
-        <img src={faker.image.avatar()} alt='avatar' />
+        <img src={props.img} alt='avatar' />
       </a>
       <div className='content'>
         <a href='/' className='author'>
--- React/components/src/index.js
+++ React/components/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-
+import faker from 'faker';
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail name='Alex' description='WOW' time='6:00PM' />
-      <CommentDetail name='Alex1' description='WOW1' time='7:00PM' />
-      <CommentDetail name='Alex2' description='WOW2' time='8:00PM' />
+      <CommentDetail
+        name='Alex'
+        description='WOW'
+        time='6:00PM'
+        img={faker.image.avatar()}
+      />
+      <CommentDetail
+        name='Alex1'
+        description='WOW1'
+        time='7:00PM'
+        img={faker.image.avatar()}
+      />
+      <CommentDetail
+        name='Alex2'
+        description='WOW2'
+        time='8:00PM'
+        img={faker.image.avatar()}
+      />
     </div>
   );
 };
